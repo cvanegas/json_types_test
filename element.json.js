e0b38fb3d8ffe834e0532a97a87f4e1edc2ba8da
cvanegas <cavanegas@gmail.com>
2017-04-26 20:24:06 -0700

    one line

--- element.json.js
+++ element.json.js
@@ -1,16 +1,1 @@
-{
-    "$schema": "http://json-schema.org/draft-04/schema#",
-    "title": "Address",
-    "properties": {
-    "street_address": {
-        "type": "string"
-    },
-    "city": {
-        "type": "string"
-    },
-    "state": {
-        "type": "string"
-    }
-},
-    "type": "object"
-}
+{"$schema":"http://json-schema.org/draft-04/schema#","title":"Address","properties":{"street_address":{"type":"string"},"city":{"type":"string"},"state":{"type":"string"}},"type":"object"}
