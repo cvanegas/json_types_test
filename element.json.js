a94c4b39ce447aa364eedbb9fb6d7ea7a1ec0499
cvanegas <cavanegas@gmail.com>
2017-04-26 20:17:34 -0700

    first

---
+++ element.json.js
@@ -0,0 +1,16 @@
+{
+    "$schema": "http://json-schema.org/draft-04/schema#",
+    "title": "Address",
+    "properties": {
+    "street_address": {
+        "type": "string"
+    },
+    "city": {
+        "type": "string"
+    },
+    "state": {
+        "type": "string"
+    }
+},
+    "type": "object"
+}
